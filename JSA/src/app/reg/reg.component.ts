3a09ffab28075b7ed69a9d8b7a4ff3aefebe13a5
HemaPriya KB <hemapriya.kb@optisolbusiness.com>
2023-03-08 16:23:39 +0530

    Register Page

---
+++ JSA/src/app/reg/reg.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg',
+  templateUrl: './reg.component.html',
+  styleUrls: ['./reg.component.css'],
+  
+})
+export class RegComponent {
+  
+  
+
+  getValues(val:string){
+    console.warn(val);
+  }
+}
