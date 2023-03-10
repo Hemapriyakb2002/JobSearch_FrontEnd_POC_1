e6f0eca4a6da2d067efd2d0180bf636b263f50db
HemaPriya KB <hemapriya.kb@optisolbusiness.com>
2023-03-10 17:35:18 +0530

    Added Routers and components

--- JSA/src/app/app-routing.module.ts
+++ JSA/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyLoginComponent } from './company-reg/company-login/company-login.component';
+import { CompanyRegComponent } from './company-reg/company-reg.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegComponent } from './reg/reg.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomepageComponent,
+    path: ''
+  },
+  {
+    component: CompanyRegComponent,
+    path: 'register'
+  },
+  {
+    component:CompanyLoginComponent,
+      path:'register/login'
+  },
+  {
+    component: RegComponent,
+    path: 'reg'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- JSA/src/app/app.module.ts
+++ JSA/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegComponent } from './reg/reg.component';
 import { FormsModule } from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CompanyRegComponent } from './company-reg/company-reg.component';
+import { CompanyLoginComponent } from './company-reg/company-login/company-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegComponent
+    RegComponent,
+    HomepageComponent,
+    CompanyRegComponent,
+    CompanyLoginComponent
   ],
   imports: [
     BrowserModule,
